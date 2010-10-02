6af425b1413d0378f4e4c9a10db7ab1084c6d194
LauraHParkinson <LauraHParkinson@03f68112-bc4a-38d4-e27f-070585ae184d>
2010-10-02 02:06:08 +0000

    Should send and receive messages from touchscreen

--- trunk/MoodKeyboard/MoodKeyboardContext/LWEvent.cs
+++ trunk/MoodKeyboard/MoodKeyboardContext/LWEvent.cs
@@ -3,6 +3,30 @@ using System.Runtime.Serialization;
 
 namespace LWEvent
 {
+    public class LWTouchPoint
+    {
+        public double x;
+        public double y;
+
+        public LWTouchPoint(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public String Serialize()
+        {
+            String s = String.Format("{0:G},{1:G}", x, y);
+            return s;
+        }
+
+        public static LWTouchPoint Deserialize(String s)
+        {
+            String[] parts = s.Split(',');
+            return new LWTouchPoint(Double.Parse(parts[0]), Double.Parse(parts[1]));
+        }
+    };
+
     public class LWEventData
     {
         public LWKey key;
